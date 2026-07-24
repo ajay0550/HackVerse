fe49047bed87ce5387da6596f011cc8a3df98a8e
ajay0550 <ajaygundu03@gmail.com>
2026-07-24 16:06:30 +0530

    signup route done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,11 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 
 export const signup = async (req, res) => {
     try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        req.body.password = hashedPassword;
         const user = await User.create(req.body);
         res.status(201).json({
             message: "User created successfully",
