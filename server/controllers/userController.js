ee27b7121fd564313b762b706875b20523bfb532
ajay0550 <ajaygundu03@gmail.com>
2026-07-24 15:29:16 +0530

    made controller logic for user signup

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,20 @@
-export const signup = async(req,res)=>{
-    res.send("Signup Route Working!");
-}
+import User from "../models/User.js";
+
+
+export const signup = async (req, res) => {
+    try{
+        const user = await User.create(req.body);
+        res.status(201).json({
+            message: "User created successfully",
+            user
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "Somthing went wrong";
+        });
+
+    }
+
+    
+};
