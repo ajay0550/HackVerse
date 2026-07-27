8e2753282b63a5fbd27b948da8f9dde98fae3edd
ajay0550 <ajaygundu03@gmail.com>
2026-07-27 15:15:30 +0530

    login route coded

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -20,5 +20,32 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req,res) =>{
-    
+    try{
+        const user = await User.findOne({
+            email : req.body.email
+        });
+        if(!user) {
+            return res.status(400).json({
+                message: "Invalid credentials"
+            });
+        }
+        const typedpassword = req.body.password;
+        const isMatch = await bcrypt.compare(typedpassword,user.password);
+        if(isMatch){
+            return res.status(200).json({
+                message: "User Logged in"
+            });
+        }
+
+        return res.status(400).json({
+            message : "Invalid credentials"
+        });
+        
+    } 
+    catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message: "Something went wrong!"
+        });
+    }
 }
