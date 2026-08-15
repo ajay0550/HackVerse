4db01dd9d4862dba358d84aa4e74d462ecf4ba4b
ajay0550 <ajaygundu03@gmail.com>
2026-08-15 15:39:02 +0530

    Connect frontend to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://hackverse-backend-gf1c.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
